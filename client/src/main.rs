93b22bd0757357e1f765f82571aa2cc9849ce31a
Vincenzo Barbato <vincenzo.barbato.51999@gmail.com>
2024-05-02 16:37:06 +0200

    Updated Client to use offer.proto

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,13 @@
 use tonic::transport::Channel;
+use rand::Rng;
 use product::{product_client::ProductClient, Empty};
+use offer::{offer_client::OfferClient, OfferRequest};
 
 pub mod product {
     tonic::include_proto!("product");
+}
+pub mod offer {
+  tonic::include_proto!("offer");
 }
 
 #[tokio::main]
@@ -13,14 +18,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   .await?;
 
     // Create a gRPC client
-    let mut client = ProductClient::new(channel);
+    let mut clientProduct = ProductClient::new(channel.clone());
+    let mut clientOffer = OfferClient::new(channel.clone());
     loop {
       // Make gRPC calls
-      let price_response = client.get_price(Empty {}).await?; // Call GetPrice with an Empty request
-      println!("Price received: {:?}", price_response.into_inner().price);
-
-      let sn_response = client.get_sn(Empty {}).await?; // Call GetSN with an Empty request
-      println!("Serial number received: {:?}", sn_response.into_inner().sn);
+      let price = (clientProduct.get_price(Empty {}).await?).into_inner().price; // Call GetPrice with an Empty request
+      let sn = (clientProduct.get_sn(Empty {}).await?).into_inner().sn; // Call GetSN with an Empty request
+      println!("Recived product with sn: {:?} and price: {:?}", sn, price);
+      
+      let new_price = rand::thread_rng().gen_range(10..=200);
+      let offer_request = OfferRequest {
+        price: ( new_price ) as i32,
+        sn: sn,
+      };
+      let confirmed = (clientOffer.confirm_offer(offer_request).await?).into_inner().confirmed; // Call ConfirmOffer with an OfferRequest
+      println!("Server response: {:?} for offer: {:?}\n", confirmed, new_price);
       tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     }
     Ok(())
